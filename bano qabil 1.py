8a72c2dc5f1ca2df1dbbc4f62bca71500371896b
AliArsalan123 <shajjiaamama67@gmail.com>
2024-10-30 20:06:04 +0500

    Add files via upload

---
+++ bano qabil 1.py
@@ -0,0 +1,9 @@
+# Initializing three numbers
+num1, num2, num3 = 20, 30, 40
+# Calculating sum and average and product and difference
+sum_numbers = num1 + num2 + num3
+average = sum_numbers / 3
+product = ("num1*num2*num3")
+diffecrence = ("num1-num2-num3")
+# Displaying the result
+print(f'Sum: {sum_numbers}, Average: {average},product:{num1*num2*num3},difference:{num1-num2-num3}')
